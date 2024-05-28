6d2cd383cb9f79ec5d21af3e7147ae0f4f69704d
Rubesh <rubeshkk28@gmail.com>
2024-05-28 16:17:08 +0530

    Update urls.py

--- fit/urls.py
+++ fit/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
-urlpatterns=[
-    path('Home/',views.Home,name='Home'),
-    path('Home/SignUp/',views.SignUp,name='SignUp'),
-    path('Home/SignIn/',views.SignIn,name='SignIn'),
-    path('Home/SignIn/SignUp/',views.SignUp,name='SignUp'),
-    path('Home/Challenges/',views.Challenges,name='Challenges'),
-    path('Home/SignIn/SignUp/CreateUser/',views.CreateUser,name='CreateUser'),
-    path('Home/SignIn/Challenges/',views.Challenges,name='Challenges'),    
+
+urlpatterns = [
+    path('', views.Home, name='Home'), 
+    path('SignUp/', views.SignUp, name='SignUp'),
+    path('SignIn/', views.SignIn, name='SignIn'),
+    path('SignIn/SignUp/', views.SignUp, name='SignUp'),
+    path('Challenges/', views.Challenges, name='Challenges'),
+    path('SignIn/SignUp/CreateUser/', views.CreateUser, name='CreateUser'),
+    path('SignIn/Challenges/', views.Challenges, name='Challenges'),
 ]
+
