1420c51cda73d0a4c6ad2ce7f9d65eb6e468a01b
Rubesh <rubeshkk28@gmail.com>
2024-05-29 15:57:24 +0530

    Update views.py

--- fit/views.py
+++ fit/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse,HttpResponseRedirect
-from django.shortcuts import render,redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
 from django.template import loader
 from fit.models import Registration
 from django.contrib.auth import authenticate, login
@@ -8,52 +8,56 @@ from django.urls import reverse
 
 # Create your views here.
 def Home(request):
-    template=loader.get_template('Home.html')
-    return HttpResponse(template.render())
+    return render(request, 'Home.html')
+
 def Challenges(request):
-    template=loader.get_template('Challenges.html')
-    return HttpResponse(template.render())
+    return render(request, 'Challenges.html')
+
 def SignUp(request):
-    template=loader.get_template('SignUp.html')
-    return HttpResponse(template.render({},request))
+    return render(request, 'SignUp.html')
 
 def SignIn(request):
-    template=loader.get_template('SignIn.html')
-    return HttpResponse(template.render({},request))
+    return render(request, 'SignIn.html')
 
 def CreateUser(request):
-    # if request.method == 'POST':
-    name=request.POST['Name']
-    uname = request.POST['Username']
-    Email = request.POST['Mail']
-    phone=request.POST['Phone']
-    Gender = request.POST['Gender']
-    Password = request.POST['Password']
-    val=Registration(full_name = name,username = uname,email = Email,phone_number = phone,gender = Gender,password=Password)
-    val.save()
-    return HttpResponseRedirect(reverse('SignIn'))
-    # else:
-    #     return render(request, 'SignUp')
+    if request.method == 'POST':
+        name = request.POST['Name']
+        uname = request.POST['Username']
+        email = request.POST['Mail']
+        phone = request.POST['Phone']
+        gender = request.POST['Gender']
+        password = request.POST['Password']
+        
+        if not all([name, uname, email, phone, gender, password]):
+            messages.error(request, "All fields are required.")
+            return redirect('SignUp')
 
-
+        # You should hash the password before saving it
+        user = Registration(full_name=name, username=uname, email=email, phone_number=phone, gender=gender, password=password)
+        user.save()
+        return HttpResponseRedirect(reverse('SignIn'))
+    else:
+        return render(request, 'SignUp.html')
 
 def Login(request):
     if request.method == 'POST':
-        try:
-            username = request.POST['Uname']
-            password = request.POST['Pass']
-        except KeyError:
-            return HttpResponse("Form data is incomplete")
+        username = request.POST.get('Uname')
+        password = request.POST.get('Pass')
+        
+        if not all([username, password]):
+            messages.error(request, "Both username and password are required.")
+            return redirect('SignIn')
+        
         try:
             user = Registration.objects.get(username=username)
             if user.password == password:
                 # Redirect to the 'challenges' page after successful authentication
-                return redirect('Challenges')  # Redirect to the challenges page
+                return redirect('Challenges')
             else:
-                return HttpResponse("Wrong password")
+                messages.error(request, "Wrong password.")
+                return redirect('SignIn')
         except Registration.DoesNotExist:
-            return HttpResponse("User does not exist")
+            messages.error(request, "User does not exist.")
+            return redirect('SignIn')
     else:
-        return HttpResponse("Invalid request method")
-
-
+        return HttpResponse("Invalid request method.")
