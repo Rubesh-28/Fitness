562252b8fb1a2042f0753d17efbf391a65dd13fc
Rubesh <rubeshkk28@gmail.com>
2024-05-31 16:05:24 +0530

    Update urls.py

--- fit/urls.py
+++ fit/urls.py
@@ -5,9 +5,7 @@ urlpatterns = [
     path('', views.Home, name='Home'), 
     path('SignUp/', views.SignUp, name='SignUp'),
     path('SignIn/', views.SignIn, name='SignIn'),
-    path('SignIn/SignUp/', views.SignUp, name='SignUp'),
     path('Challenges/', views.Challenges, name='Challenges'),
-    path('SignIn/SignUp/CreateUser/', views.CreateUser, name='CreateUser'),
-    path('SignIn/Challenges/', views.Challenges, name='Challenges'),
+    path('CreateUser/', views.CreateUser, name='CreateUser'),
+    path('Login/', views.Login, name='Login'),
 ]
-
