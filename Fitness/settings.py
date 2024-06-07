74eb560da293e6b49b5c31ade1033c61c9705f53
Rubesh <rubeshkk28@gmail.com>
2024-06-07 16:18:38 +0530

    Update settings.py

--- Fitness/settings.py
+++ Fitness/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ccqef9jqjf!d&e&*110vhtmg!^o0qlptjrsk*o808^x9_@y&gg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
